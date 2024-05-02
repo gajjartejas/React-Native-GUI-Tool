9275fb923dfb62690c81a9fedd35f68676d89e17
Tejas Gajjar <gajjartejas26@gmail.com>
2024-05-02 23:03:03 +0530

    feat: set window tiitle

---
+++ ReactNativeGUITools/Controller/MainProjectListWC.swift
@@ -0,0 +1,20 @@
+//
+//  MainProjectListWC.swift
+//  ReactNativeGUITools
+//
+//  Created by Tejas on 02/05/24.
+//
+
+import Cocoa
+
+class MainProjectListWC: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        
+        self.window?.title = "React Native GUI Tools"
+    }
+
+}
