e5ad5dad15ffb91e9736d92c52cab28a82e7df4e
Tejas Gajjar <gajjartejas26@gmail.com>
2024-06-25 23:15:58 +0530

    feat: add webstorm beta and zed to editor option to path settings (#83)

--- ReactNativeGUITools/Modal/MainSettings/MainSettings.swift
+++ ReactNativeGUITools/Modal/MainSettings/MainSettings.swift
@@ -10,11 +10,13 @@ import Foundation
 enum AppBundleIdentifier: String, Codable {
     case vscode = "com.microsoft.VSCode"
     case webStorm = "com.jetbrains.WebStorm"
+    case webStormEAP = "com.jetbrains.WebStorm-EAP"
     case atom = "com.github.atom"
     case sublimeText = "com.sublimetext.4"
     case nova = "com.panic.Nova"
     case xcode = "com.apple.dt.Xcode"
     case androidStudio = "com.google.android.studio"
+    case zed = "dev.zed.Zed"
 }
 
 extension AppBundleIdentifier {
@@ -24,6 +26,8 @@ extension AppBundleIdentifier {
             return "Visual Studio Code"
         case .webStorm:
             return "WebStorm"
+        case .webStormEAP:
+            return "WebStorm(EAP)"
         case .atom:
             return "Atom"
         case .sublimeText:
@@ -34,6 +38,8 @@ extension AppBundleIdentifier {
             return "Xcode"
         case .androidStudio:
             return "Android Studio"
+        case .zed:
+            return "Zed"
         }
     }
 }
@@ -71,22 +77,22 @@ class MainSettings: Codable, PlistStorable {
 
     func getCodeEditorsaths() -> [PathVersion] {
         let paths = getAppPathsAndVersions(
-            for: ["visual", "studio", "code", "webstorm", "atom", "sublime", "text", "nova"],
-            withBundleIdentifiers: ["com.microsoft.VSCode", "com.jetbrains.WebStorm", "com.github.atom", "com.sublimetext.4", "com.panic.Nova"])
+            for: ["visual", "studio", "code", "webstorm", "atom", "sublime", "text", "nova", "zed"],
+            withBundleIdentifiers: [.vscode, .webStorm, .atom, .sublimeText, .nova, .webStormEAP, .zed])
         return paths
     }
 
     func getXcodesPaths() -> [PathVersion] {
-        let paths = getAppPathsAndVersions(for: ["xcode"], withBundleIdentifiers: ["com.apple.dt.Xcode"])
+        let paths = getAppPathsAndVersions(for: ["xcode"], withBundleIdentifiers: [.xcode])
         return paths
     }
 
     func getAndroidStudioPaths() -> [PathVersion] {
-        let paths = getAppPathsAndVersions(for: ["android", "studio"], withBundleIdentifiers: ["com.google.android.studio"])
+        let paths = getAppPathsAndVersions(for: ["android", "studio"], withBundleIdentifiers: [.androidStudio])
         return paths
     }
 
-    func getAppPathsAndVersions(for appNames: [String], withBundleIdentifiers bundleIdentifiers: [String]) -> [PathVersion] {
+    func getAppPathsAndVersions(for appNames: [String], withBundleIdentifiers bundleIdentifiers: [AppBundleIdentifier]) -> [PathVersion] {
         let fileManager = FileManager.default
         let applicationsDirectory = "/Applications"
         var appPathsAndVersions = [PathVersion]()
@@ -97,8 +103,8 @@ class MainSettings: Codable, PlistStorable {
                 if appNames.contains(where: { item.lowercased().contains($0.lowercased()) }) && item.hasSuffix(".app") {
                     let fullPath = "\(applicationsDirectory)/\(item)"
                     if let bid = isApp(at: fullPath, withBundleIdentifiers: bundleIdentifiers),
-                       let version = getAppVersion(at: fullPath), let bidValue = AppBundleIdentifier(rawValue: bid) {
-                        appPathsAndVersions.append(PathVersion(path: fullPath, version: version, bundleId: bidValue))
+                       let version = getAppVersion(at: fullPath) {
+                        appPathsAndVersions.append(PathVersion(path: fullPath, version: version, bundleId: bid))
                     }
                 }
             }
@@ -114,12 +120,10 @@ class MainSettings: Codable, PlistStorable {
         return bundle?.infoDictionary?["CFBundleShortVersionString"] as? String
     }
 
-    private func isApp(at path: String, withBundleIdentifiers bundleIdentifiers: [String]) -> String? {
+    private func isApp(at path: String, withBundleIdentifiers bundleIdentifiers: [AppBundleIdentifier]) -> AppBundleIdentifier? {
         let bundle = Bundle(path: path)
         if let bundleIdentifier = bundle?.infoDictionary?["CFBundleIdentifier"] as? String {
-            if bundleIdentifiers.contains(bundleIdentifier) {
-                return bundleIdentifier
-            }
+            return bundleIdentifiers.first(where: { $0.rawValue == bundleIdentifier })
         }
         return nil
     }
