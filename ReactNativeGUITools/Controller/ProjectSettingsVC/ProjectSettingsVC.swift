fa5814c65afd5c36c122482c3b2d9624aeca689d
Tejas Gajjar <gajjartejas26@gmail.com>
2024-06-30 18:25:18 +0530

    feat: add project setting options in project info window

---
+++ ReactNativeGUITools/Controller/ProjectSettingsVC/ProjectSettingsVC.swift
@@ -0,0 +1,24 @@
+//
+//  ProjectSettingsVC.swift
+//  ReactNativeGUITools
+//
+//  Created by Tejas on 30/06/24.
+//
+
+import Cocoa
+
+class ProjectSettingsVC: NSViewController {
+    // MARK: - Properties
+
+    // MARK: - Lifecycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Setup
+
+    // MARK: - Actions
+
+    // MARK: - Helpers
+}
